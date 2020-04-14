325ce0b54ae5b0888863fa825e94e79c2abd4726
XuChao <xuchao_1213@163.com>
2020-04-14 20:53:56 +0800

    add run as windows service support

--- Wechat.Api/Program.cs
+++ Wechat.Api/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using NLog;
 using NLog.Web;
 using System;
+using Topshelf;
 
 namespace Wechat.Api
 {
@@ -11,6 +12,25 @@ namespace Wechat.Api
     {
         public static void Main(string[] args)
         {
+            var rc = HostFactory.Run(x =>
+            {
+                x.Service<MainServices>(sc => {
+                    sc.ConstructUsing(name => new MainServices(args));
+                    sc.WhenStarted((s, hc) => s.Start(hc));
+                    sc.WhenStopped((s, hc) => s.Stop(hc));
+                    sc.WhenShutdown((s, hc) => s.Shutdown(hc));
+                });
+                x.SetDescription("wechat helper service for puietel ssm");
+                x.SetDisplayName("wechat-helper");
+                x.SetServiceName("wechat-helper");
+                x.EnableServiceRecovery(c => {
+                    c.RestartService(1);
+                });
+                x.StartAutomatically();
+                x.RunAsLocalSystem();
+            });
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());
+            Environment.ExitCode = exitCode;
             var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
             try
             {
@@ -33,7 +53,7 @@ namespace Wechat.Api
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+            Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
